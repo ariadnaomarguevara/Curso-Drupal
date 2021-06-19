9d7b9297679febff3c2829a414efd5fcac46c2ba
Omar Guevara <omar.guevara@ariadnacg.com>
2021-06-19 10:04:24 -0500

    servicios en drupal

---
+++ web/modules/custom/curso_formulario/src/Dependecia.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\curso_formulario;
+
+/**
+ * Dependecia service.
+ */
+class Dependecia {
+
+  /**
+   * Method description.
+   */
+  public function doSomething() {
+    // @DCG place your code here.
+  }
+
+}
---
+++ web/modules/custom/curso_formulario/src/Example.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\curso_formulario;
+
+/**
+ * Example service.
+ */
+class Example {
+
+  /**
+   * Method description.
+   */
+  public function doSomething() {
+    print "Hola";
+  }
+
+}
